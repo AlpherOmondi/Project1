bd33b0b611f03e514ad4e11064840a913e76102f
AlphaOmondi <alphaomondi001@gmail.com>
2022-07-10 21:04:52 +0300

    Implemented seperation of concerns and completed all model relationships

---
+++ models.py
@@ -0,0 +1,73 @@
+#----------------------------------------------------#
+#IMPORTS
+#----------------------------------------------------#
+
+from flask import Flask
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+import collections
+collections.Callable = collections.abc.Callable
+
+#----------------------------------------------------#
+#CONFIGURATION
+#----------------------------------------------------#
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+#----------------------------------------------------#
+#MODELS
+#----------------------------------------------------#
+
+#Venue model
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120), db.ARRAY(db.String()), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    talent_searching = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='Venue', lazy='joined')
+
+
+#Artist
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120), db.ARRAY(db.String()), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venues = db.Column(db.Boolean, default=True)
+    seeking_description = db.Column(db.String(120), default="I'm looking for a venue")
+    shows = db.relationship('Show', backref='Artist', lazy='joined')
+
+
+#Shows
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+    show_id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
